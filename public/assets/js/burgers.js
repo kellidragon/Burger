fd2f14153a6faa47a61a6a1552040541c2ae770c
kellidragon <dragonkelli@gmail.com>
2019-06-14 19:09:40 -0700

    burgers update and devour button works, but isnt displaying

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,25 +1,25 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-    // $(".eatenOrNot").on("click", function(event) {
-    //   var id = $(this).data("id");
-    //   var newBurger = $(this).data("newburger");
+    $(".devourBtn").on("click", function(event) {
+      var id = $(this).data("id");
+      var newBurger = $(this).data("newburger");
   
-    //   var newBurgerEntered = {
-    //     burger_name: newBurger
-    //   };
+      var newBurgerEntered = {
+        devoured: newBurger
+      };
   
-    //   // Send the PUT request.
-    //   $.ajax("/api/burgers/" + id, {
-    //     type: "PUT",
-    //     data: newBurgerEntered
-    //   }).then(
-    //     function() {
-    //       console.log("new burger added", newBurger);
-    //       // Reload the page to get the updated list
-    //       location.reload();
-    //     }
-    //   );
-    // });
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newBurgerEntered
+      }).then(
+        function() {
+          console.log("new burger devoured", newBurger);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
   
     $(".create-form").on("submit", function(event) {
       // Make sure to preventDefault on a submit event.
@@ -39,8 +39,6 @@ $(function() {
         function() {
           console.log("created new burger");
           
-        //   var deleteBtn = ('<button>')
-        //   $("#burg").append(deleteBtn);
           // Reload the page to get the updated list
           location.reload();
         }
