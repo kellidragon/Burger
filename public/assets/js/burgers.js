66ec783af708c89d994028cb4dc8201cb1fbe847
kellidragon <dragonkelli@gmail.com>
2019-06-16 13:35:23 -0700

    basic layout created

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,5 +1,10 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
+
+  $(".letsPlay").on("click", function(event) {
+    $(".jumbotron").hide();
+  });
+
     $(".devourBtn").on("click", function(event) {
       var id = $(this).data("id");
       var newDevour = $(this).data("newdevour");
